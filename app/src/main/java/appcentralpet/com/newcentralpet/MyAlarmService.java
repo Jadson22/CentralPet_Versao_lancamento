63f0f02c9a0807b3a4000ac03205963d84593873
Jadson22 <jadsonufc@gmail.com>
2017-10-12 16:45:35 -0300

    iniciando a apanhar pra notificação

---
+++ app/src/main/java/appcentralpet/com/newcentralpet/MyAlarmService.java
@@ -0,0 +1,72 @@
+package appcentralpet.com.newcentralpet;
+
+/**
+ * Created by Jadson on 12/10/2017.
+ */
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+
+public class MyAlarmService extends Service
+{
+
+    private NotificationManager mManager;
+
+    @Override
+    public IBinder onBind(Intent arg0)
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        // TODO Auto-generated method stub
+        super.onCreate();
+    }
+
+    @SuppressWarnings("static-access")
+    @Override
+    public void onStart(Intent intent, int startId)
+    {
+        super.onStart(intent, startId);
+
+        Notification.Builder builder = new Notification.Builder(MyAlarmService.this);
+        Intent notificationIntent = new Intent(this,NavigationDrawer.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,notificationIntent, 0);
+        builder.setSmallIcon(R.drawable. notification_template_icon_bg)
+                .setContentTitle("Central Pet")
+                .setContentText("Seu pet tem umma vacina agendada para hoje")
+                .setContentIntent(pendingIntent);
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        Notification notification = builder.getNotification();
+        notificationManager.notify(R.drawable.notification_template_icon_bg, notification);
+
+        //mManager = (NotificationManager) this.getApplicationContext().getSystemService(this.getApplicationContext().NOTIFICATION_SERVICE);
+        //Intent intent1 = new Intent(this.getApplicationContext(),NavigationDrawer.class);
+
+
+        /*Notification notification = new Notification(R.drawable.cast_ic_notification_small_icon,"This is a test message!", System.currentTimeMillis());
+        intent1.addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP| Intent.FLAG_ACTIVITY_CLEAR_TOP);
+
+        PendingIntent pendingNotificationIntent = PendingIntent.getActivity( this.getApplicationContext(),0, intent1,PendingIntent.FLAG_UPDATE_CURRENT);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        notification.(this.getApplicationContext(),"Central Pet", "Seu Pet tem uma vacina agendada para hoje", pendingNotificationIntent);
+
+        mManager.notify(0, notification);
+        */
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+    }
+
+}
